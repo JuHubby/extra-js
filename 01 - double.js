2e209fbc17466f9d8e73271d3698aee04b5a043c
Nigar <movsum.nigar@gmail.com>
2023-01-25 16:32:00 +0100

    Added explanation for the first task - double.js

--- 01 - double.js
+++ 01 - double.js
@@ -1,7 +1,39 @@
 // create a double function that will receive a number as an argument, and return the number multiplied by 2
 
-const double = () => {
- 
+const double = (num) => {
+    return num * 2
 };
 
-console.log(double(800));
+// const double = (num) => {
+//     num = num * 2
+//     return num
+// };
+
+// console.log(double(800));
+// let primitiveNum = 5
+// console.log(primitiveNum)
+// let result = double(primitiveNum)
+// console.log("result = " + result)
+// console.log(primitiveNum)
+
+
+function changeName(human) {
+    human.name = "Nigar"
+    return human
+}
+
+let person = {
+    "name": "Jason"
+}
+let person1 = person
+
+console.log(person)
+console.log(person1)
+let person2 = changeName(person);
+
+console.log(person)
+console.log(person1)
+console.log(person2)
+
+
+
